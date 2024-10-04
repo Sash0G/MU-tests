c24bad1251d7fe4e26cdd3022da879b89c1a36b1
Sasho_G <100850331+Sash0G@users.noreply.github.com>
2024-10-05 01:01:35 +0300

    First Commit

--- app/src/main/java/com/example/mu_tests/SecondActivity.kt
+++ app/src/main/java/com/example/mu_tests/SecondActivity.kt
@@ -1,6 +1,8 @@
 package com.example.mu_tests
 
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,23 @@ class SecondActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val myButton: Button = findViewById(R.id.myButton)
+        var isClicked = false
+        myButton.setOnClickListener {
+
+            if (isClicked) {
+                // Change back to the original state
+                myButton.text = "Click Me"
+                myButton.setBackgroundColor(Color.BLUE)
+            } else {
+                // Change to clicked state
+                myButton.text = "Clicked!"
+                myButton.setBackgroundColor(Color.RED)
+            }
+
+            // Toggle the flag
+            isClicked = !isClicked
         }
     }
 }
